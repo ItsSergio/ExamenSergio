ed985acee5fab439bdb48065ec0bee9f3617a6a9
ItsSergio <guerrerosergio320@gmail.com>
2022-04-27 09:13:23 +0200

    Primer Commit hecho de Suma

---
+++ prueba1/src/prueba1/Suma.java
@@ -0,0 +1,22 @@
+package prueba1;
+
+import javax.swing.JOptionPane;
+
+public class Suma {
+
+	public static void main(String[] args) {
+		int var, var2;
+		String str; 
+		
+		str = JOptionPane.showInputDialog("Primer número para sumar: ");
+		var = Integer.parseInt(str);
+		
+		str = JOptionPane.showInputDialog("Segundo número para sumar");
+		var2 = Integer.parseInt(str);
+		
+		int suma = var + var2;
+		System.out.println("Suma de los dos números es: " + suma);
+
+	}
+
+}
